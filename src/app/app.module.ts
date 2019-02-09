7fd9fc7dff5be50f8d7e5df2e10174b511d17e22
Moises Rojas <moidrh@gmail.com>
2019-02-09 17:31:02 -0500

    Tercer commit - Se realizaron cambios de personalización al Auth y de permisos a la página protegida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProtegidaComponent } from './components/protegida/protegida.component';
 import { ROUTING } from './app.routes';
 
 import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ import { AuthService } from './services/auth.service';
     BrowserModule,
     ROUTING
   ],
-  providers: [ AuthService ],
+  providers: [ AuthService, AuthGuardService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
